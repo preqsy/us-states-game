eb3e5aa48e5af5ab9b3c64be27078ab9e4257635
Precious Obinna <obbyprecious24@gmail.com>
2023-09-11 23:10:35 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S States Games")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+guessed_state = []
+data = pandas.read_csv("50_states.csv")
+# answer_state = turtle.textinput(title="Guess The U.S State", prompt="Guess another state").capitalize()
+t = turtle.Turtle()
+while len(guessed_state) < 50:
+    answer_state = turtle.textinput(title=f"{len(guessed_state)}/50", prompt="Guess another state").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in data.state:
+            if state not in guessed_state:
+                missing_states.append(state)
+        df = pandas.DataFrame(missing_states)
+        df.to_csv("missed_states.csv")
+            
+        break
+    if answer_state in str(data.state):
+        if answer_state in guessed_state:
+            pass
+        else:
+            guessed_state.append(answer_state)
+        state_data = data[data.state == answer_state]
+        new_x = state_data.x
+        state_data = data[data.state == answer_state]
+        new_y = state_data.y
+        print(type(int(new_x)))
+        t.penup()
+        t.goto(int(new_x), int(new_y))
+        t.write(f"{answer_state}")
+        
+
+        
